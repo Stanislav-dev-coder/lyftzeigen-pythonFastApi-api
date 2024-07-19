be618aedcc9991f467739a27af0e22487ee5b29c
Stanislav-dev-coder <stanislavbajkov2@gmail.com>
2024-07-19 23:09:28 +0300

    init

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+from pymongo import MongoClient
 
 app = FastAPI()
+
+client = MongoClient("mongodb://mongodb.lyftzeigen.ru:28017/")
+db = client['randomTest']
+collection = db['indexRandomColletion']
 
 @app.get("/fastapi")
 async def root():
