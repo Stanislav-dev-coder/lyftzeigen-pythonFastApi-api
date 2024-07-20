58f4dfe9661ae632d929f387ec052d326b97255d
Stanislav-dev-coder <stanislavbajkov2@gmail.com>
2024-07-20 18:33:30 +0300

    test

--- db/dbConfig.py
+++ db/dbConfig.py
@@ -5,9 +5,13 @@ MONGODB_URL = os.environ.get("MONGODB_URL")
 MONGODB_USER = os.environ.get("MONGODB_USER")
 MONGODB_PASS = os.environ.get("MONGODB_PASS")
 
-client = MongoClient(MONGODB_URL,
-                     username=MONGODB_USER,
-                     password=MONGODB_PASS)
+client = MongoClient(
+    MONGODB_URL,
+    username=MONGODB_USER,
+    password=MONGODB_PASS,
+    authSource='rndDB',
+    authMechanism='SCRAM-SHA-256'
+)
 
 db = client["rndDB"]
 
