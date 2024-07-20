e87fe0eea0dde985c862420581b660480af346ba
Stanislav-dev-coder <stanislavbajkov2@gmail.com>
2024-07-21 00:59:23 +0300

    test

--- db/dbConfig.py
+++ db/dbConfig.py
@@ -1,25 +1,19 @@
 from pymongo import MongoClient
 import os
-from urllib.parse import quote_plus
 
+# MONGODB_HOST = "mongodb.lyftzeigen.ru"
+# MONGODB_PORT = "28017"
+# MONGODB_USER = "testUser"
+# MONGODB_PASS = "root1234"
+# MONGODB_AUTH_DB = "rndDB"
 
-#MONGODB_URL = os.environ.get("MONGODB_URL")
-#$MONGODB_USER = os.environ.get("MONGODB_USER")
-#MONGODB_PASS = os.environ.get("MONGODB_PASS")
-#MONGODB_COLLECT = os.environ.get("MONGODB_COLLECT")
+MONGODB_HOST = os.environ.get("MONGODB_HOST")
+MONGODB_PORT = os.environ.get("MONGODB_PORT")
+MONGODB_USER = os.environ.get("MONGODB_USER")
+MONGODB_PASS = os.environ.get("MONGODB_PASS")
+MONGODB_AUTH_DB = os.environ.get("MONGODB_AUTH_DB")
 
-MONGODB_HOST = "mongodb.lyftzeigen.ru"
-MONGODB_PORT = 28017
-MONGODB_USER = "testUser"
-MONGODB_PASS = "root1234"  # Замените "root1234" на ваш реальный пароль
-MONGODB_AUTH_DB = "rndDB"
-
-# Формируем строку подключения
-uri = "mongodb://%s:%s@%s:%s/?authSource=%s" % (
-    quote_plus(MONGODB_USER), quote_plus(MONGODB_PASS), quote_plus(MONGODB_HOST), MONGODB_PORT, quote_plus(MONGODB_AUTH_DB)
-)
-
-client = MongoClient(uri)
+client = MongoClient(f"mongodb://{MONGODB_USER}:{MONGODB_PASS}@{MONGODB_HOST}:{MONGODB_PORT}/?directConnection=true&authSource={MONGODB_AUTH_DB}")
 
 db = client["rndDB"]
 
