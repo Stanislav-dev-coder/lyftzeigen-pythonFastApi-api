f7acd35ebabb50d8a2c4baa5a7516d3b8d0669a5
Stanislav-dev-coder <stanislavbajkov2@gmail.com>
2024-07-21 00:28:05 +0300

    test

--- db/dbConfig.py
+++ db/dbConfig.py
@@ -1,18 +1,12 @@
 from pymongo import MongoClient
 import os
-from urllib.parse import quote_plus
-
 
 MONGODB_URL = os.environ.get("MONGODB_URL")
 MONGODB_USER = os.environ.get("MONGODB_USER")
 MONGODB_PASS = os.environ.get("MONGODB_PASS")
 MONGODB_COLLECT = os.environ.get("MONGODB_COLLECT")
 
-uri = "mongodb://%s:%s@%s/%s" % (
-    quote_plus(str(MONGODB_USER)), quote_plus(str(MONGODB_PASS)), quote_plus(str(MONGODB_URL)), quote_plus(str(MONGODB_COLLECT))
-)
-
-client = MongoClient(uri)
+client = MongoClient(f"mongodb://{MONGODB_USER}:{MONGODB_PASS}@{MONGODB_URL}/{MONGODB_COLLECT}")
 
 db = client["rndDB"]
 
