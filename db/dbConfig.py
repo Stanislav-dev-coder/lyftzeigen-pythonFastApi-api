03b32d5d62902488c0bdf83839051f7b8a88d7ce
root <root@3192965-lyftzeigen.twc1.net>
2024-07-21 01:04:40 +0300

    test

--- db/dbConfig.py
+++ db/dbConfig.py
@@ -13,7 +13,13 @@ MONGODB_USER = os.environ.get("MONGODB_USER")
 MONGODB_PASS = os.environ.get("MONGODB_PASS")
 MONGODB_AUTH_DB = os.environ.get("MONGODB_AUTH_DB")
 
-client = MongoClient(f"mongodb://{MONGODB_USER}:{MONGODB_PASS}@{MONGODB_HOST}:{MONGODB_PORT}/?directConnection=true&authSource={MONGODB_AUTH_DB}")
+uri = "mongodb://%s:%s@%s:%s/?authSource=%s" % (
+    quote_plus(MONGODB_USER), quote_plus(MONGODB_PASS), quote_plus(MONGODB_HOST), quote_plus(MONGODB_PORT), quote_plus(MONGODB_AUTH_DB)
+)
+
+client = MongoClient(uri)
+
+#client = MongoClient(f"mongodb://{MONGODB_USER}:{MONGODB_PASS}@{MONGODB_HOST}:{MONGODB_PORT}/?directConnection=true&authSource={MONGODB_AUTH_DB}")
 
 db = client["rndDB"]
 
